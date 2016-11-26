14c37d0ae6d9c37060a4fa95a766c3c3c92d7860
Long Ma <t_mal@fenqi.im>
2016-11-26 17:09:50 +0800

    "first commit  for create base layout"

---
+++ src/client/controllers/application/application.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: '../../views/application/application.component.html',
+    styleUrls: '../../css/application/application.component.coss'
+})
+export class AppComponent {
+    title = 'app works!';
+}
